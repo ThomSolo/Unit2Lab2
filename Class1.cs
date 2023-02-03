5b76eba5345684b289e1042cb3d0180ff57748e8
thomsolo <thomsolo@kean.edu>
2023-02-03 11:56:23 -0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿
+//Name: Solomon Thomas, Eyitayo Akinjinmi, and Riley Powers
+//Course Section: CPS*3330*01
+//Lab #2
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace KeanID_Library_Unit2
+{
+    public class Class1
+    {
+        public static double Min(double num1, double num2)
+        {
+            if (num1 < num2) { return num1; }
+            else { return num2; }
+        }
+
+        public static double Min(double num1, double num2, double num3)
+        {
+            double min = num1;
+            if (num2 < min) { min = num2; }
+            if (num3 < min) { min = num3; }
+            return min;
+        }
+    }
+}
+
